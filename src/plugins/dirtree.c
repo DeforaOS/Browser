68c386f4cf6745b43628a4f4680102261fc2087e
Pierre Pronchery <khorben@defora.org>
2015-10-26 23:30:48 +0100

    Code cleanup

--- src/plugins/dirtree.c
+++ src/plugins/dirtree.c
@@ -142,7 +142,7 @@ static Dirtree * _dirtree_init(BrowserPluginHelper * helper)
 	renderer = gtk_cell_renderer_text_new();
 	column = gtk_tree_view_column_new_with_attributes(NULL, renderer,
 			"text", DC_NAME, NULL);
-	gtk_tree_view_column_set_sort_column_id(column, 1);
+	gtk_tree_view_column_set_sort_column_id(column, DC_NAME);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(dirtree->view), column);
 	/* selection */
 	treesel = gtk_tree_view_get_selection(GTK_TREE_VIEW(dirtree->view));
