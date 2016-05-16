939a97ee95a4b998a87ca565fa1ab3de6ab38405
Pierre Pronchery <khorben@defora.org>
2016-05-16 03:49:42 +0200

    Update the copyright notice

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Id$
-#Copyright (c) 2013-2015 Pierre Pronchery <khorben@defora.org>
+#Copyright (c) 2013-2016 Pierre Pronchery <khorben@defora.org>
 #This file is part of DeforaOS Desktop Browser
 #Redistribution and use in source and binary forms, with or without
 #modification, are permitted provided that the following conditions are
