471e5c802200d4c517e008e0090783070830d1d1
Pierre Pronchery <khorben@defora.org>
2026-06-17 17:16:33 -0400

    src/desktop: assume /usr/local as PREFIX on FreeBSD
    This is now consistent with what the menu from DeforaOS Panel does,
    which should work best when running as a package when on FreeBSD.
    The real solution will be to detect duplicate desktop entries at
    run-time though.

--- src/desktop/handler/categories.c
+++ src/desktop/handler/categories.c
@@ -350,7 +350,10 @@ static void _categories_on_refresh_loop_xdg(DesktopHandler * handler,
 	/* read through every XDG application folder */
 	if((path = getenv("XDG_DATA_DIRS")) == NULL || strlen(path) == 0)
 	{
-#if defined(__NetBSD__)
+#if defined(__FreeBSD__)
+		/* XXX really detect if DATADIR is already included */
+		path = DATADIR ":/usr/share";
+#elif defined(__NetBSD__)
 		/* XXX include the default path for pkgsrc */
 		path = "/usr/pkg/share:" DATADIR ":/usr/share";
 #else
